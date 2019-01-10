6ee64e3996f18492424d789b37c15b94ab3079ce
vdepetigny <vdepetigny@yahoo.fr>
2019-01-10 14:59:28 +0100

    mise à jour 01 et 02

--- 01_pyramids.rb
+++ 01_pyramids.rb
@@ -1,5 +1,21 @@
+#Une demi-pyramide
+
+def half_pyramid 
+	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+	print "> "
+	user_floors = gets.to_i
+
+	for floor in 1..user_floors
+		blank = " " * (user_floors - floor)
+		diese = "#" * (floor)
+		puts blank + diese
+	end
+end
+
+#Une full pyramide
+
 def full_pyramid
-	puts "Nombre d'étage ?"
+	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 	print "> "
 	user_floors = gets.to_i
 
@@ -8,12 +24,30 @@ def full_pyramid
 		diese = "#" * (floor+floor-1)
 		puts blank + diese
 	end
-
-	for floor in 1..(user_floors-1)
-		blank = " " * (floor)
-		diese = "#" * (user_floors+user_floors-floor-floor-1)
-		puts blank + diese
-	end
 end
 
+#Un losange
+
+def wtf_pyramid
+	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu (nombre impair) ?"
+	print "> "
+	user_floors = gets.to_i
+	middle = (user_floors/2) + 1
+
+	for floor in 1..(middle)
+		blank = " " * (middle - floor)
+		diese = "#" * (floor+floor-1)
+		puts blank + diese
+	 end
+
+	for floor in 1..(middle-1)
+	 	blank = " " * (floor)
+	 	diese = "#" * (user_floors-floor-floor)
+	 	puts blank + diese
+	 end
+
+end
+
+half_pyramid
 full_pyramid
+wtf_pyramid
--- 02_password.rb
+++ 02_password.rb
@@ -1,11 +1,77 @@
 #le programme va demander à l'utilisateur de définir 
 #un mot de passe et le garder en mémoire (en le stockant dans une variable).
 
-def signup 
-	puts "Entrez votre mot de passe"
+# def signup 
+# 	puts "Définissez votre mot de passe"
+# 	print "> "
+# 	mdpTrue = gets.chomp
+# 	return mdpTrue
+# end
+
+# def login
+# 	puts "Entrez votre mot de passe"
+# 	prints "> "
+# 	mdp = gets.chomp
+# 	return mdp
+# end
+	
+# def greet (mdpTrue, mdp)
+# 	puts "Bienvenue, #{mdpTrue} #{mdp} !"
+# end
+
+
+# # 	if mdp = mdpTrue
+# # 		puts "Vous pouvez entrer"
+# # 	else
+# # 	puts "Entrez votre mot de passe de nouveau"
+# # 	prints "> "
+# # 	mdp = gets.chomp
+# # 	end
+# # end
+
+# def perform
+# 	mdpTrue = signup
+# 	mdp = login
+# 	greet(mdpTrue, mdp)
+# end
+
+# perform
+
+
+#Introduction à l'usage des méthodes
+
+#demander le prénom
+
+def ask_password
+	puts "Définissez votre password"
 	print "> "
-	mdpTrue = gets.chomp
-	signup = {"u" => "mdpTrue"}
-end 
+	true_mdp = gets.chomp
+	return true_mdp
+end
 
+#demander le nom de famille
+def sign_in
+	puts "Quel est votre password?"
+	print "> "
+	mdp = gets.chomp
+	return mdp
+end
 
+#prononcer le message de bienvenue
+def greet (true_mdp,mdp)
+	while mdp != true_mdp
+		puts "Entrez votre mot de passe de nouveau"
+ 		print "> "
+  		mdp = gets.chomp
+	end
+	puts "Vous pouvez entrer"
+		
+end
+
+def perform
+	true_mdp = ask_password
+	mdp = sign_in
+	greet(true_mdp,mdp)
+end
+
+perform

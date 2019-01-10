fd3372bb29c94718d24b738292a2df9d980ac149
vdepetigny <vdepetigny@yahoo.fr>
2019-01-10 10:39:49 +0100

    Exo 1 complété - viviane

---
+++ 00_hello.rb
@@ -0,0 +1,19 @@
+#say hello
+
+def ask_first_name
+	puts "Quel est ton prénom?"
+	print "> "
+	first_name = gets.chomp
+	return first_name
+end
+
+def say_hello(first_name)
+	puts "Hello #{first_name}!"
+end
+
+def perform
+	first_name = ask_first_name
+	say_hello(first_name)
+end
+
+perform
